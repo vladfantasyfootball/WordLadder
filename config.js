e62898d271d985399ae3e963c42a1ed872cab5df
Vladimir <vladfantasyfootball@gmail.com>
2026-02-20 14:55:44 -0600

    feat: setting up to use railway

--- config.js
+++ config.js
@@ -10,8 +10,21 @@ import {
 	GOOGLE_WEB_CLIENT_ID,
 	EXPO_CLIENT_ID,
 	IOS_CLIENT_ID,
-	ANDROID_CLIENT_ID
+	ANDROID_CLIENT_ID,
+	WORD_LADDER_BACKEND
 } from '@env';
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
+
+// Use localhost in development, Railway in production
+const getBackendUrl = () => {
+	if (__DEV__) {
+		// Development builds use localhost
+		return Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+	}
+	// Production builds use Railway
+	return WORD_LADDER_BACKEND;
+};
 
 export default {
 	API_KEY,
@@ -24,5 +37,6 @@ export default {
 	GOOGLE_WEB_CLIENT_ID,
 	EXPO_CLIENT_ID,
 	IOS_CLIENT_ID,
-	ANDROID_CLIENT_ID
+	ANDROID_CLIENT_ID,
+	WORD_LADDER_BACKEND: getBackendUrl()
 };
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,11 +1,11 @@
 import { USER_STATE_CHANGE, WORD_LADDER_CHANGE } from '../constants/index';
 import axios from 'axios';
-import { Platform } from 'react-native';
+import config from '../../config';
 
 export function fetchUser(auth) {
     return (async (dispatch) => {
         if (auth.currentUser) {
-            await axios.post(Platform.OS === 'ios' ? 'http://localhost:3000/api/getUser' : 'http://10.0.2.2:3000/api/getUser', { id: auth.currentUser.uid }).then((response) => {
+            await axios.post(`${config.WORD_LADDER_BACKEND}/api/getUser`, { id: auth.currentUser.uid }).then((response) => {
                 let userData = response.data;
                 dispatch({ type: USER_STATE_CHANGE, currentUser: userData })
             }).catch((e) => {
@@ -20,7 +20,7 @@ export function fetchUser(auth) {
 
 export function getWordLadders() {
     return (async (dispatch) => {
-        await axios.get(Platform.OS === 'ios' ? 'http://localhost:3000/api/getPuzzles' : 'http://10.0.2.2:3000/api/getPuzzles').then((response) => {
+        await axios.get(`${config.WORD_LADDER_BACKEND}/api/getPuzzles`).then((response) => {
             dispatch({
                 type: WORD_LADDER_CHANGE,
                 wordLadder: { "one": response.data.one, "two": response.data.two }
@@ -33,7 +33,7 @@ export function getWordLadders() {
 
 export function updateUser(id, userUpdate) {
     return (async (dispatch) => {
-        await axios.post(Platform.OS === 'ios' ? 'http://localhost:3000/api/updateUser' : 'http://10.0.2.2:3000/api/updateUser', {id, userUpdate: userUpdate}).then((response) => {
+        await axios.post(`${config.WORD_LADDER_BACKEND}/api/updateUser`, {id, userUpdate: userUpdate}).then((response) => {
             dispatch({
                 type: USER_STATE_CHANGE,
                 currentUser: userUpdate
