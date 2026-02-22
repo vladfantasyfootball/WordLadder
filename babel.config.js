4b7b394cf6cc9a33f0fc3be666e528626a56e82e
Vladimir <vladfantasyfootball@gmail.com>
2026-02-21 18:19:05 -0600

    Fix babel preset for Expo

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    "presets": ["module:@react-native/babel-preset"],
-    "plugins": [
+    presets: ['babel-preset-expo'],
+    plugins: [
       ["module:react-native-dotenv", {
         "moduleName": "@env",
         "path": ".env",
