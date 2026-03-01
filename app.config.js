4cf8cb0a68cad3adeae5e13adf7518cd177e1cc7
Vladimir <vladfantasyfootball@gmail.com>
2026-02-28 19:35:14 -0600

    Add app.config.js to inject EAS secrets into production builds

---
+++ app.config.js
@@ -0,0 +1,89 @@
+export default {
+  expo: {
+    name: "WordLadder",
+    slug: "WordLadder",
+    version: "2",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    extra: {
+      API_KEY: process.env.API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+      APP_ID: process.env.APP_ID,
+      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+      GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+      EXPO_CLIENT_ID: process.env.EXPO_CLIENT_ID,
+      IOS_CLIENT_ID: process.env.IOS_CLIENT_ID,
+      ANDROID_CLIENT_ID: process.env.ANDROID_CLIENT_ID,
+      WORD_LADDER_BACKEND: process.env.WORD_LADDER_BACKEND
+    },
+    ios: {
+      googleServicesFile: "./GoogleService-Info.plist",
+      bundleIdentifier: "com.vlad.wordLadder",
+      buildNumber: "3",
+      supportsTablet: true,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+            deploymentTarget: "15.1"
+          }
+        }
+      ]
+    ],
+    android: {
+      googleServicesFile: "./google-services.json",
+      package: "com.vlad.wordLadderAndroid",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    scheme: "wordladder",
+    owner: "vladfantasyfootball",
+    "react-native-google-mobile-ads": {
+      android_app_id: "ca-app-pub-9598423455146889~1026066069",
+      ios_app_id: "ca-app-pub-9598423455146889~7821518890"
+    },
+    extra: {
+      eas: {
+        projectId: "93b26197-abcf-46f3-bff1-d9b23e1b7e55"
+      },
+      API_KEY: process.env.API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+      APP_ID: process.env.APP_ID,
+      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+      GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+      EXPO_CLIENT_ID: process.env.EXPO_CLIENT_ID,
+      IOS_CLIENT_ID: process.env.IOS_CLIENT_ID,
+      ANDROID_CLIENT_ID: process.env.ANDROID_CLIENT_ID,
+      WORD_LADDER_BACKEND: process.env.WORD_LADDER_BACKEND
+    }
+  }
+};
